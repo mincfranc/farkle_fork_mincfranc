d13af9ba5dc26f386404f742793984b223545e28
Bill <wholcombe@cnm.edu>
2025-03-09 20:52:21 -0600

    User Repository Links file moved to correct folder

--- server/src/main/java/edu/cnm/deepdive/farkle/model/dao/GameRepository.java
+++ server/src/main/java/edu/cnm/deepdive/farkle/model/dao/GameRepository.java
@@ -1,8 +1,11 @@
 package edu.cnm.deepdive.farkle.model.dao;
 
 import edu.cnm.deepdive.farkle.model.entity.Game;
+import edu.cnm.deepdive.farkle.model.entity.User;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface GameRepository extends JpaRepository<Game, Long>{
 
+  List<Game> findByUserProfile(User user);
 }
