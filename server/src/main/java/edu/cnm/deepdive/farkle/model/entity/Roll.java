287510812e0a08a21c368606833af3ab199097b8
Aaron Halbig <aaronhalbig@gmail.com>
2025-03-21 11:47:20 -0600

    Integrating changes into roll

--- server/src/main/java/edu/cnm/deepdive/farkle/model/entity/Roll.java
+++ server/src/main/java/edu/cnm/deepdive/farkle/model/entity/Roll.java
@@ -1,5 +1,7 @@
 package edu.cnm.deepdive.farkle.model.entity;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonProperty.Access;
 import jakarta.persistence.AttributeOverride;
 import jakarta.persistence.AttributeOverrides;
 import jakarta.persistence.CollectionTable;
@@ -12,8 +14,12 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+import java.time.Instant;
 import java.util.LinkedList;
 import java.util.List;
+import org.hibernate.annotations.CreationTimestamp;
 
 @Entity
 public class Roll {
@@ -21,26 +27,31 @@ public class Roll {
   @Id
   @GeneratedValue
   @Column(name = "roll_id")
-  private Long Id;
+  private long Id;
 
   @Column(nullable = true)
   private int rollScore;
 
-  @Column(nullable = false, updatable = false)
+  @Column(nullable = false, updatable = true)
   private boolean farkle;
 
   @Column(nullable = false, updatable = false)
   private int numberDice;
 
-  // TODO: 3/20/25 Add a relationship to turn
-  @JoinColumn(nullable = false, updatable = false)
+  @JoinColumn(name = "turn_id", nullable = false, updatable = false)
   @ManyToOne(fetch = FetchType.LAZY, optional = false)
   private Turn turn;
+
+  @Column(nullable = false)
+  @CreationTimestamp
+  @Temporal(TemporalType.TIMESTAMP)
+  @JsonProperty(access = Access.READ_ONLY)
+  private Instant timestamp;
 
   @ElementCollection
   @CollectionTable(name = "roll_die", joinColumns = @JoinColumn(name = "roll_id"))
   @AttributeOverrides(
-      @AttributeOverride(name=  "value", column = @Column(name = "face_value", nullable = false, updatable = false))
+      @AttributeOverride(name = "value", column = @Column(name = "face_value", nullable = false, updatable = false))
   )
   private final List<Die> dice = new LinkedList<>();
 
@@ -56,6 +67,14 @@ public class Roll {
     this.rollScore = rollScore;
   }
 
+  public boolean isFarkle() {
+    return farkle;
+  }
+
+  public void setFarkle(boolean farkle) {
+    this.farkle = farkle;
+  }
+
   public int getNumberDice() {
     return numberDice;
   }
@@ -68,10 +87,24 @@ public class Roll {
     return dice;
   }
 
+  public Turn getTurn() {
+    return turn;
+  }
+
+  public void setTurn(Turn turn) {
+    this.turn = turn;
+  }
+
+  public Instant getTimestamp() {
+    return timestamp;
+  }
+
   @Embeddable
   public static class Die {
 
     private int value;
+
+    private int group;
 
     public int getValue() {
       return value;
@@ -79,6 +112,14 @@ public class Roll {
 
     public void setValue(int value) {
       this.value = value;
+    }
+
+    public int getGroup() {
+      return group;
+    }
+
+    public void setGroup(int group) {
+      this.group = group;
     }
   }
 
