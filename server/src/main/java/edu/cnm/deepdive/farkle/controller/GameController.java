28946d4b850287eb3bee378de3a30c31f50d43ed
Aaron Halbig <aaronhalbig@gmail.com>
2025-03-21 11:48:28 -0600

    Working on RollAction and service implementations

--- server/src/main/java/edu/cnm/deepdive/farkle/controller/GameController.java
+++ server/src/main/java/edu/cnm/deepdive/farkle/controller/GameController.java
@@ -1,13 +1,19 @@
 package edu.cnm.deepdive.farkle.controller;
 
 
+import edu.cnm.deepdive.farkle.model.dto.RollAction;
 import edu.cnm.deepdive.farkle.model.entity.Game;
 import edu.cnm.deepdive.farkle.service.AbstractGameService;
 import edu.cnm.deepdive.farkle.service.AbstractUserService;
+import java.util.UUID;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -28,6 +34,10 @@ public class GameController {
     return gameService.startOrJoin(userService.getCurrent());
   }
 
-
+  @PostMapping(path = "/{key}/actions", consumes = MediaType.APPLICATION_JSON_VALUE)
+  @ResponseStatus(HttpStatus.NO_CONTENT)
+  public void freezeOrContinue(@RequestBody RollAction action, @PathVariable UUID key) {
+    gameService.freezeOrContinue(action, key, userService.getCurrent());
+  }
 
 }
--- server/src/main/java/edu/cnm/deepdive/farkle/service/GameService.java
+++ server/src/main/java/edu/cnm/deepdive/farkle/service/GameService.java
@@ -1,11 +1,13 @@
 package edu.cnm.deepdive.farkle.service;
 
 import edu.cnm.deepdive.farkle.model.dao.GameRepository;
+import edu.cnm.deepdive.farkle.model.dto.RollAction;
 import edu.cnm.deepdive.farkle.model.entity.Game;
 import edu.cnm.deepdive.farkle.model.entity.State;
 import edu.cnm.deepdive.farkle.model.entity.User;
 import java.util.EnumSet;
 import java.util.List;
+import java.util.UUID;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -38,7 +40,19 @@ public class GameService implements AbstractGameService {
               return gameRepository.save(game);
             })
         );
-    // Do we need to do .findByUserProfile?
+  }
+
+  @Override
+  public void freezeOrContinue(RollAction action, UUID key, User user) {
+
+    // TODO: 3/21/25 Query game object with key and User 
+    // TODO: 3/21/25 Look at most recent turn and most recent roll in game object
+    // TODO: 3/21/25 Validate whether the user IS the current user in turn
+    // TODO: 3/21/25 Validate what the user wants to do is valid according to the most recent roll
+    // TODO: 3/21/25 Compute the score that results from this action
+    // TODO: 3/21/25 Update the turn and roll entity instances and write to the database
+    // TODO: 3/21/25 Check if this turn puts the game in 'last round' state
+    // TODO: 3/21/25 If turn, advance to the next turn and user
   }
 
   @Override
